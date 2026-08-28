e7d29fb02a29cf34e66e33360d20a85c65d0d4ac
Jit2005git <jitghosh304@gmail.com>
2026-08-28 20:27:07 +0530

    p1

---
+++ Primitives.java
@@ -0,0 +1,13 @@
+public class Primitives {
+    public static void main(String[] args) {
+        // int rollno=71;
+        // char letter='j';
+        // float marks=90.5f;
+        // double largeDecimalNumbers=123456789.123456789;
+        // long largeInteger=1234567890L;
+        // boolean check=true;
+
+        
+}}
+
+
