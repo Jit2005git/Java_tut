591b9ea250b57f9dc939e1e3cc0225b267cea63c
Jit2005git <jitghosh304@gmail.com>
2026-08-29 19:31:32 +0530

    input

---
+++ input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args){
+        Scanner input =new Scanner(System.in);
+        System.out.print("Please Enter some input: ");
+        int rollno =input.nextInt();
+        System.out.println("Your Roll number is: "+rollno);
+    }
+
+}
+       
