ef13a6b209b33aca93f1823e65920372331db01a
Jit2005git <jitghosh304@gmail.com>
2026-09-12 12:50:02 +0530

    s1

--- conditionals&Loops/Main.java
+++ conditionals&Loops/Main.java
@@ -1,5 +1,17 @@
 public class Main{
     public static void main(String [] args){
         
+        int salary = 10000;
+        if(salary > 10000){
+            salary = salary + 2000;
+            System.out.println("You are a rich person");
+
+        }
+        else{
+            salary = salary + 1000;
+            System.out.println("You are a poor person");
+        }
+        System.out.println("Your salary is: " + salary);
+
     }
 }
