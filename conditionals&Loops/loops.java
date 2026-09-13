13cb5a9166ac8b06ba1686def2850dcc96c7d517
Jit2005git <jitghosh304@gmail.com>
2026-09-13 13:20:36 +0530

    l5

--- conditionals&Loops/loops.java
+++ conditionals&Loops/loops.java
@@ -11,8 +11,13 @@ public class loops{
 //print number from 1 to n
 Scanner in = new Scanner(System.in);
 int n=in.nextInt();
+System.out.println("The value of n is: " + n);
+printNumbers(n);
+
+    }
+    static void printNumbers(int n){
 for (int i=1;i<=n;i++){
-    System.out.println(i);
+    System.out.println("The value of i is: " + i);
      
 }
 
