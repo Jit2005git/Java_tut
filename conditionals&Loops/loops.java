18e66c704b8291143105196207ecf9df82f385c2
Jit2005git <jitghosh304@gmail.com>
2026-09-13 13:12:00 +0530

    l2

--- conditionals&Loops/loops.java
+++ conditionals&Loops/loops.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
 public class loops{
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("The value of i is: " + i);
-        }
+//         for (int i = 0; i < 10; i += 2) {
+//             System.out.println("The value of i is: " + i);
+//         }
 
+
+
+
+//print number from 1 to n
+Scanner in = new Scanner(System.in);
+int n=in.nextInt();
+for (int i=1;i<=n;i++){
+    System.out.println(i);
+     
 }
+
+    }
 }
