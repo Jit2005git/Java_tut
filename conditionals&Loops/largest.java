90cccc616fd37427baa3202ef405a30ce30f5622
Jit2005git <jitghosh304@gmail.com>
2026-09-20 21:02:18 +0530

    l1

---
+++ conditionals&Loops/largest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class largest{
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int a=in.nextInt();
+        System.out.print("Enter the second number: ");
+        int b=in.nextInt();
+        System.out.print("Enter the third number: ");
+        int c=in.nextInt();
+    }
+}
