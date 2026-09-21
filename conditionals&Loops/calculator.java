8cd792efe73df19f6cea1a1ab74263c77fe675f8
Jit2005git <jitghosh304@gmail.com>
2026-09-21 13:47:35 +0530

    cal2

---
+++ conditionals&Loops/calculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter two numbers and an operator (+, -, *, /):");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        char op = in.next().trim().charAt(0);
+        if(op=='+'){
+            System.out.println("The sum is: " + (a+b));
+        }
+        else if(op=='-'){
+            System.out.println("The difference is: " + (a-b));
+        }
+        else if(op=='*'){
+            System.out.println("The product is: " + (a*b));
+        }
+        else if(op=='/'){
+            if(b==0){
+                System.out.println("Division by zero is not allowed");
+            }
+            else{
+                System.out.println("The quotient is: " + (a/b));
+            }
+        }
+        else{
+            System.out.println("Invalid operator");
+        }
+        
+    }
+}
