ff1df04f535a95229d7964a5d047dcf115dc52e4
Jit2005git <jitghosh304@gmail.com>
2026-09-21 13:04:40 +0530

    c1

---
+++ conditionals&Loops/caseCheck.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class caseCheck {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    char ch = in.next().trim().charAt(0);
+    // String word="hello";
+    // // System.out.println("The character is: " + ch);
+    // System.out.println("The character is: " + word.charAt(2));
+        
+
+    }
+}
