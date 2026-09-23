6d41644cf820019f42bb125632f3e9ef2535e3d3
Jit2005git <jitghosh304@gmail.com>
2026-09-23 23:59:30 +0530

    sw2

--- switch_Case/main.java
+++ switch_Case/main.java
@@ -1,6 +1,8 @@
 package switch_Case;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        
+        Scanner in= new Scanner (System.in);
+    }
 }
